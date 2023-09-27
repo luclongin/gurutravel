b7d3ca9d9fbe5dea1c66a9e62336f124b999802f
Luc <luc.longin@gmail.com>
2023-09-27 13:32:32 -0500

    creating data js for all treks and climbs creating first version of the expedition card

---
+++ src/assets/data/allServices.js
@@ -0,0 +1,86 @@
+export const allClimbs = [
+	{
+		id: 1,
+		type: "climb",
+		name: "tullparaju mountain",
+		difficulty: "advanced",
+		altitude: "5,830M",
+		duration: "4-6 days",
+		description:
+			"Embark on a technical climb, rewarded with awe-inspiring vistas of the surrounding peaks.",
+	},
+	{
+		id: 2,
+		type: "climb",
+		name: "alpamayo mountain",
+		difficulty: "advanced",
+		altitude: "5,947M",
+		duration: "12-14 days",
+		description:
+			"One of the world's most beautiful peaks, with its challenging ascent and rewarding views.",
+	},
+	{
+		id: 3,
+		type: "climb",
+		name: "pisco mountain",
+		difficulty: "moderate",
+		altitude: "5,732M",
+		duration: "3-4 days",
+		description:
+			"Introduction to high-altitude climbing and stunning vistas of the Cordillera Blanca.",
+	},
+	{
+		id: 4,
+		type: "climb",
+		name: "huascar&aacute;n mountain",
+		difficulty: "advanced",
+		altitude: "6,768M",
+		duration: "7-10 days",
+		description:
+			"The tallest peak in Peru, offering a significant mountaineering experience.",
+	},
+];
+
+
+export const allTreks = [
+	{
+		id: 5,
+		type: "trek",
+		name: "laguna 69",
+		difficulty: "beginner",
+		altitude: "4,600M",
+		duration: "1 day",
+		description:
+			"A short but rewarding hike, a stunning turquoise glacial lake surrounded by towering peaks.",
+	},
+	{
+		id: 6,
+		type: "trek",
+		name: "huayhuash circuit",
+		difficulty: "advanced",
+		altitude: "5,000M",
+		duration: "10-12 days",
+		description:
+			"Embark on an epic journey through the remote mountain range, known for its beauty and high passes.",
+	},
+	{
+		id: 7,
+		type: "trek",
+		name: "pastoruri glacier trek",
+		difficulty: "beginner",
+		altitude: "5,000M",
+		duration: "1 day",
+		description:
+			"A short but fascinating trek where you can witness the effects of glacial retreat.",
+	},
+	{
+		id: 8,
+		type: "trek",
+		name: "santa cruz trek",
+		difficulty: "moderate",
+		altitude: "5,000M",
+		duration: "12-14 days",
+		description:
+			"Experience the beauty of Cordillera Blanca. Pristine valleys, glacial lakes, and towering peaks.",
+	},
+];
